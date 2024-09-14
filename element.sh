a4e6b563c7bc21ba02f0a25a6d28b6e25eb266ec
Tahsin Haider <chtahsinhaider@gmail.com>
2024-09-14 20:50:02 +0000

    Initial commit

---
+++ element.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$1")
+    echo $NAME
+  fi
+
+  if [[ $1 =~  ]]
+  then
+  fi
+fi
