e4d53179561accbe2c5687926ecf50a76c06c129
Tahsin Haider <chtahsinhaider@gmail.com>
2024-09-14 21:52:53 +0000

    test: input

--- element.sh
+++ element.sh
@@ -1,65 +1,46 @@
 #!/bin/bash
-PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
 
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
 
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
 else
+  # Check if the argument is an atomic number
   if [[ $1 =~ ^[0-9]+$ ]]
   then
-    NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$1" | xargs)
-    if [[ -z $NAME ]]
-    then
-     echo "I could not find that element in the database."
-    else
-      NUM=$1
-      SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE name='$NAME'" | xargs)
-      TYPE=$($PSQL "SELECT type FROM types RIGHT JOIN properties USING(type_id) WHERE atomic_number=$NUM" | xargs)
-      MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$NUM" | xargs)
-      MP=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$NUM" | xargs)
-      BP=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$NUM" | xargs)
-
-      echo -e "\nThe element with atomic number $NUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MP celsius and a boiling point of $BP celsius."
-    fi
-  fi
-
-
-  if [[ $1 =~ ^[A-Z][a-z]?$ ]]
+    CONDITION="atomic_number=$1"
+  # Check if the argument is a symbol
+  elif [[ $1 =~ ^[A-Z][a-z]?$ ]]
   then
-    NAME=$($PSQL "SELECT name FROM elements WHERE symbol='$1'" | xargs)
-    if [[ -z $NAME ]]
-    then
-     echo "I could not find that element in the database."
-    else
-      SYMBOL=$1
-      NUM=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$SYMBOL'" | xargs)
-      TYPE=$($PSQL "SELECT type FROM types RIGHT JOIN properties USING(type_id) WHERE atomic_number=$NUM" | xargs)
-      MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$NUM" | xargs)
-      MP=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$NUM" | xargs)
-      BP=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$NUM" | xargs)
-
-      echo -e "\nThe element with atomic number $NUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MP celsius and a boiling point of $BP celsius."
-    fi
-  fi
-
-  if [[ $1 =~ ^[A-Z][a-z][a-z]+$ ]]
+    CONDITION="symbol='$1'"
+  # Check if the argument is an element name
+  elif [[ $1 =~ ^[A-Za-z]+$ ]]
   then
-    NUM=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1'" | xargs)
-
-    if [[ -z $NUM ]]
-    then
-      echo "I could not find that element in the database."
-    else
-      NAME=$1
-      MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number='$NUM'" | xargs)
-      TYPE=$($PSQL "SELECT type FROM types RIGHT JOIN properties USING(type_id) WHERE atomic_number=$NUM" | xargs)
-      SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$NUM" | xargs)
-      MP=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$NUM" | xargs)
-      BP=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$NUM" | xargs)
-
-      echo -e "\nThe element with atomic number $NUM is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MP celsius and a boiling point of $BP celsius."
-    fi
+    CONDITION="name='$1'"
+  else
+    echo "I could not find that element in the database."
+    exit
   fi
 
+  # Get element details
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol FROM elements WHERE $CONDITION")
+  
+  # Check if element was found
+  if [[ -z $ELEMENT ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    # Parse the retrieved element data
+    IFS=" | " read ATOMIC_NUMBER NAME SYMBOL <<< $(echo $ELEMENT | xargs)
+    
+    # Get other properties from the database
+    TYPE=$($PSQL "SELECT type FROM types RIGHT JOIN properties USING(type_id) WHERE atomic_number=$ATOMIC_NUMBER" | xargs)
+    MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$ATOMIC_NUMBER" | xargs)
+    MP=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER" | xargs)
+    BP=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER" | xargs)
+
+    # Display the element information
+    echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MP celsius and a boiling point of $BP celsius."
+  fi
 fi
